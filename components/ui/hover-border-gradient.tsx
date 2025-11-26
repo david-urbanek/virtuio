0488cbe35036524253f3c3136b7712eda194cb68
David Urbanek <urbanek.dav@email.cz>
2025-11-26 13:39:21 +0100

    refactor: Initialize `useRef` with null and broaden `as` prop type to `any`.

--- components/ui/hover-border-gradient.tsx
+++ components/ui/hover-border-gradient.tsx
@@ -16,7 +16,7 @@ export function HoverBorderGradient({
   ...props
 }: React.PropsWithChildren<
   {
-    as?: React.ElementType;
+    as?: any;
     containerClassName?: string;
     className?: string;
     duration?: number;
